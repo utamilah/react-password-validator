a5b43f746d35d224bb0eb336781f952735e99be7
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-05-26 13:57:06 -0400

    add email validation

--- src/Validator.js
+++ src/Validator.js
@@ -5,10 +5,18 @@ class Validator extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      email: '',
+      emailValid: '',
       passwordInput: '',
       confirmPasswordInput: '',
       passwordsMatch: false
     }
+  }
+
+  handleEmail(e) {
+    this.setState({
+      email: e.target.value
+    })
   }
 
   handlePassword(e) {
@@ -33,20 +41,32 @@ class Validator extends Component {
           passwordsMatch: false
         })
       }
+
+      if(this.state.email.includes('@') === true ) {
+        this.setState({
+          emailValid: true
+        })
+      } else {
+        this.setState({
+          emailValid: false
+        })
+      }
   }
 
 
 
   render() {
     var passwordsMatch = this.state.passwordsMatch ? "Passwords Match!" : "Passwords do not match"
+    var emailValid = this.state.email.includes('@') ? "Email is valid!" : "Email is invalid"
     return (
       <div className="form">
         <h1>Sign Up</h1>
-        <input type="text" placeholder="email" />
+        <input type="text" placeholder="email" onChange={(e) => this.handleEmail(e)}/>
         <input type="password" placeholder="password" onChange={(e) => this.handlePassword(e)}/>
         <input type="password" placeholder="confirm password" onChange={(e) => this.handleConfirmPassword(e)}/>
         <input type="submit" value="Submit"  onClick={(e) => this.handlePasswordInputChange(e)} />
-        {passwordsMatch}
+        <p>{passwordsMatch}</p>
+        <p>{emailValid}</p>
       </div>
     );
   }
